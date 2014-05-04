80872cd6204d1e9c0f184504299ab0626185747c
Air-Mark <air.t.mark@gmail.com>
2014-05-05 03:13:57 +0400

    cng

--- assets/scripts.js
+++ assets/scripts.js
@@ -19,7 +19,8 @@
 		// Create a countdown instance. Change the launchDay according to your needs.
 		// The month ranges from 0 to 11. I specify the month from 1 to 12 and manually subtract the 1.
 		// Thus the launchDay below denotes 7 May, 2014.
-		var launchDay = new Date(2014, 5-1, 7);
+		var launchDay = new Date();
+		launchDay.setDate(launchDay.getDate()+29); 
 		$('#timer').countdown({
 			until: launchDay
 		});
